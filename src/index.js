07bb9049052999fa8fcde977f5f553e31c76ddf2
Hosein <hosein2398@gmail.com>
2017-05-30 15:09:04 +0330

    coverage

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function doit(){
+    var vt = "this is a hello world";
+    return "hi";
+}
+function dont(){
+    console.log("saef")
+}
+module.exports = doit;
--- test/test.js
+++ test/test.js
@@ -1,5 +1,10 @@
+var doit = require("../src/index.js");
+
 test('two plus two is four', () => {
   expect(2 + 2).toBe(4);
+}); 
+test('doit', () => {
+  expect(doit()).toBe("hi");
 }); 
 
 test('object assignment', () => {
